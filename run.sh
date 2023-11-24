5faea94764aeb089f177ae4bc6de1b428968485d
trustytrojan <tarbot24@hotmail.com>
2023-11-23 22:18:38 -0500

    simple push

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-KEY_CERT_PATH=/etc/letsencrypt/live/trustytrojan.dev
+KEY_CERT_PATH=/etc/letsencrypt/live/chat.trustytrojan.dev
 node chat-server.js 443 $KEY_CERT_PATH/privkey.pem $KEY_CERT_PATH/fullchain.pem &>log & echo $! >pidfile
