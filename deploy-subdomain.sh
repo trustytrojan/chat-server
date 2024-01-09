9c9edf3260be9c3db8994428e5fb7c123d8e8506
trustytrojan <tarbot24@hotmail.com>
2024-01-08 20:51:56 -0500

    Remove deploy-proxy.sh and deploy-subdomain.sh scripts

--- deploy-subdomain.sh
+++
@@ -1,3 +0,0 @@
-kill $(cat pid)
-KEY_CERT_PATH=/etc/letsencrypt/live/chat.trustytrojan.dev
-node . 443 wss://chat.trustytrojan.dev $KEY_CERT_PATH/privkey.pem $KEY_CERT_PATH/fullchain.pem &>log & echo $! >pid
